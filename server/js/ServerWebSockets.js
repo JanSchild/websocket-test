de9dfdd1643a4d741f8e58944a344452e0c3b956
Jan Schild <60048667+JanSchild@users.noreply.github.com>
2025-07-12 00:21:56 +0200

    ServerWebSockets.js aktualisieren

--- server/js/ServerWebSockets.js
+++ server/js/ServerWebSockets.js
@@ -1,5 +1,4 @@
 import WebSocket, { WebSocketServer } from 'ws';
-import { v4 as uuidv4 } from 'uuid';
 import { Player } from '../../shared/Player.js';
 import { ServerPlayersManager } from './ServerPlayersManager.js';
 import { IncomingMessage } from './IncomingMessage.js';
@@ -12,22 +11,14 @@ export class ServerWebSockets {
         ServerWebSockets.wss = new WebSocketServer({ server });
 
         ServerWebSockets.wss.on('connection', function connection(ws) {
-            let id = uuidv4();
-            let x = Math.random() * 400;
-            let y = Math.random() * 400;
-            let width = 30;
-            let height = 30;
-            let speed = 5;
-            let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-            let player = new Player(id, x, y, width, height, speed, color);
+            let player = ServerPlayersManager.generatePlayer();
             ServerPlayersManager.addPlayer(player);
-            ws.id = id;
+            ws.id = player.id;
 
             console.log(`Player joined`);
             let initMsg = { type: 'init', data: { playerID: id, players: ServerPlayersManager.totalPlayersData() } };
             ws.send(JSON.stringify(initMsg));
 
-            // Notify others of new player
             let playerJoinedMsg = { type: 'player_joined', data: { player } };
             ServerWebSockets.broadcast(playerJoinedMsg);
 
